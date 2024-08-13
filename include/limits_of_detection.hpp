2d139dedd8b48073a1cd0d4f58f6b640693a785a
Kevin <kevt19@utexas.edu>
2024-08-13 09:53:02 -0600

    added exceptions to functions

--- include/limits_of_detection.hpp
+++ include/limits_of_detection.hpp
@@ -16,11 +16,6 @@ namespace LOD {
  * @param[in] LLD [float] Facility Detection Policy, or in other words, the Lower Limit of Detection. Units: (DPM/cm^2)
  * 
  * @return [float] Activity level that is deemed as a detection using a detector with a specified size. Units: (DPS)
- * 
- * @throws TODO:ExceptionType Description of any exceptions that might be thrown.
- * 
- * @note TODO:Additional notes or remarks about the function.
- * @warning TODO:Any warnings or important considerations.
  */
 float calculateActivityLLD(float detector_surface_area, float LLD);
 
@@ -37,7 +32,7 @@ float calculateActivityLLD(float detector_surface_area, float LLD);
  * 
  * @return [float] Dwell time, the minimum amount of time required to dwell over a surface. Units: (s)]
  * 
- * @throws TODO:ExceptionType Description of any exceptions that might be thrown.
+ * @throws std::invalid_argument: The 'efficiency' parameter has to be within the range of (0-1)
  * 
  * @note TODO:Additional notes or remarks about the function.
  * @warning TODO:Any warnings or important considerations.
@@ -58,10 +53,8 @@ float calculateDwellTime(float activityLLD, float efficiency, float relative_std
  * 
  * @return [float] Ideal detector velocity.
  * 
- * @throws TODO:ExceptionType Description of any exceptions that might be thrown.
- * 
- * @note TODO:Additional notes or remarks about the function.
- * @warning TODO:Any warnings or important considerations.
+ * @throws std::invalid_argument: The 'efficiency' parameter has to be within the range of (0-1)
+ * @throws std::invalid_argument: The 'relative_std' parameter has to be within the range of (0-1)
  */
 float calculateDetectorVelocity(float activityLLD, float efficiency, float detector_length, float relative_std); 
 
@@ -72,16 +65,16 @@ float calculateDetectorVelocity(float activityLLD, float efficiency, float detec
  *
  * Calculates the critical limit of detection. This limit is used to assess a potential contaminated surface, however might be a fluke due to radiation and its probabilistic nature. 
  * 
- * @param[in] alpha [float] Confidence user-defined parameter that considers false positive counts. Range: (0-50).
+ * @param[in] alpha [float] Confidence user-defined parameter that considers false positive counts. Range: [0-50].
  * @param[in] bckg_count_rate [float] The room's background count rate. Units: (cps)
  * @param[in] bckg_count_time [float] The room's time used for calculating the background count rate. Units: (s).  A longer count time creates a more precise background count rate.
  * 
  * @return [float] The critical limit used for assessing a surface as potentially contaminated. Think of it as the first red flag. Units: (cps)
  * 
- * @throws TODO:ExceptionType Description of any exceptions that might be thrown.
+ * @throws std::invalid_argument: The 'alpha' parameter has to be within the range of [0-50)
+ * @throws std::invalid_argument: The 'bckg_count_rate' parameter can't be negative
+ * @throws std::invalid_argument: The 'bckg_count_time' parameter can't be negative
  * 
- * @note TODO:Additional notes or remarks about the function.
- * @warning TODO:Any warnings or important considerations.
  */
 float calculateLC(float alpha, float bckg_count_rate, float bckg_count_time);
 
@@ -93,18 +86,19 @@ float calculateLC(float alpha, float bckg_count_rate, float bckg_count_time);
  * Calculates the detection limit. A surface with a count rate above this limit will be deemed as contaminated.
  * 
  * @param[in] Lc [float] The critical limit used for assessing a surface as potentially contaminated. Think of it as the first red flag. Units: (cps)
- * @param[in] alpha [float] Confidence user-defined parameter that considers false positive counts. Range: (0-50).
- * @param[in] beta [float] Confidence user-defined parameter that considers false negative counts. Range: (0-50).
+ * @param[in] alpha [float] Confidence user-defined parameter that considers false positive counts. Range: [0-50].
+ * @param[in] beta [float] Confidence user-defined parameter that considers false negative counts. Range: [0-50].
  * @param[in] measured_dwell_time [float] The time spent over a surface. Different than the dwell time, since dwell time is a recommnded time, while this is a active measurement. Units: (s)
  * 
  * @return [float] A detection limit that marks the count rate at which a surface will be considered contaminated. Units: (cps)
  * 
- * @throws TODO:ExceptionType Description of any exceptions that might be thrown.
+ * @throws std::invalid_argument: The 'alpha' parameter has to be within the range of [0-50)
+ * @throws std::invalid_argument: The 'beta' parameter has to be within the range of [0-50)
+ * @throws std::invalid_argument: The 'measured_dwell_time' value can't be negative
  * 
- * @note TODO:Additional notes or remarks about the function.
- * @warning TODO:Any warnings or important considerations.
  */
 float calculateLD(float Lc, float alpha, float beta, float measured_dwell_time);
+
 
 }   // LOD namespace
 
--- src/limits_of_detection.cpp
+++ src/limits_of_detection.cpp
@@ -2,32 +2,88 @@
 #include <cmath>
 #include <boost/math/distributions/normal.hpp>
 #include <iostream>
+#include <stdexcept>
 
 namespace LOD {
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 float calculateActivityLLD(float detector_surface_area, float LLD){
     return (detector_surface_area*LLD)/60;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 float calculateDwellTime(float activityLLD, float efficiency, float relative_std){
+    if (efficiency <= 0 || efficiency >= 1)
+    {
+        throw std::invalid_argument("[calculateDwellTime()]: 'efficiency' parameter is outside of the acceptable range, (0-1)");
+    }
     return 1 / (activityLLD * efficiency * std::pow(relative_std,2));
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 float calculateDetectorVelocity(float activityLLD, float efficiency, float detector_length, float relative_std){
+    if (efficiency <= 0 || efficiency >= 1)
+    {
+        throw std::invalid_argument("[calculateDetectorVelocity()]: 'efficiency' parameter is outside of the acceptable range, (0-1)");
+    }
+
+    if (relative_std <= 0 || relative_std >= 1)
+    {
+        throw std::invalid_argument("[calculateDetectorvelocity()]: 'relative_std' parameter is outside of the acceptable range, (0-1)");
+    }
     return (activityLLD * efficiency * detector_length* std::pow(relative_std,2));
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 float calculateLC(float alpha, float bckg_count_rate, float bckg_count_time){
+    if (alpha < 0 || alpha >= 50)
+    {
+        throw std::invalid_argument("[calculateLC()]: 'alpha' parameter is outside of the acceptable range, [0-50)");
+    }
+
+    if (bckg_count_rate < 0)
+    {
+        throw std::invalid_argument("[calculateLC()]: 'bckg_count_rate' parameter can't be negative");
+    }
+
+    if (bckg_count_time < 0)
+    {
+        throw std::invalid_argument("[calculateLC()]: 'bckg_count_time' parameter can't be negative");
+    }
+
     const boost::math::normal_distribution<> norm_dist(0, 1);
     const double K_alpha = std::abs(boost::math::quantile(norm_dist, alpha/100));
+
     return bckg_count_rate + (K_alpha * sqrt(bckg_count_rate/bckg_count_time));
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 float calculateLD(float Lc, float alpha, float beta, float measured_dwell_time){
+    if (alpha < 0 || alpha >= 50)
+    {
+        throw std::invalid_argument("[calculateLD()]: 'alpha' parameter is outside of the acceptable range, [0-50)");
+    }
+
+    if (beta < 0 || beta >= 50)
+    {
+        throw std::invalid_argument("[calculateLD()]: 'beta' parameter is outside of the acceptable range, [0-50)");
+    }
+
+    if (measured_dwell_time < 0)
+    {
+        throw std::invalid_argument("[calculateLD()]: 'measured_dwell_time' parameter can't be negative");
+    }
+
     const boost::math::normal_distribution<> norm_dist(0, 1);
     const double K_alpha = std::abs(boost::math::quantile(norm_dist, alpha/100));
     const double K_beta = std::abs(boost::math::quantile(norm_dist, beta/100));
@@ -40,5 +96,9 @@ float calculateLD(float Lc, float alpha, float beta, float measured_dwell_time){
 
     return Lc + first * (second_third + sqrt(second_third + fourth + fifth));
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 }   // LOD namespace
